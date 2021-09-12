97f7827965f6f68cf5379c6f19868209f7182fce
Haritha Manikonda <haritha.manikonda@gmail.com>
2021-09-11 23:23:50 -0700

    test

--- src/test/java/com/testingframework/testCases/TC_LoginTest_001.java
+++ src/test/java/com/testingframework/testCases/TC_LoginTest_001.java
@@ -27,7 +27,7 @@ public class TC_LoginTest_001 extends BaseClass
 		logger.info("entered the password");
 		lp.clickSubmit();
 		logger.info("clicked the login button");
-		if(driver.getTitle().equals("Guru99rrrr Bank Manager HomePage"))
+		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
 		{
 			Assert.assertTrue(true);
 			logger.info("Login test passed");
